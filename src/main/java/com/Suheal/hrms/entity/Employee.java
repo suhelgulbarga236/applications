91a256d08d4752b40627696c50a4ee16ed3b54d6
suhelgulbarga236 <suhelshaik236@gmail.com>
2023-10-31 16:39:12 +0530

    added employee

---
+++ src/main/java/com/Suheal/hrms/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.Suheal.hrms.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee {
+
+
+}
