d7f031eb0cb64ec504f7b7cca2b7390e708f0acf
suhelgulbarga236 <suhelshaik236@gmail.com>
2023-10-31 17:26:07 +0530

    add logger

--- src/main/java/com/Suheal/hrms/service/EmployeeServiceImpl.java
+++ src/main/java/com/Suheal/hrms/service/EmployeeServiceImpl.java
@@ -2,21 +2,29 @@ package com.Suheal.hrms.service;
 
 import com.Suheal.hrms.entity.Employee;
 import com.Suheal.hrms.repo.EmployeeRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
     @Autowired
     EmployeeRepo employeeRepo;
 
     @Override
     public Employee create(Employee employee) {
+        log.info("create method " + employee);
         return employeeRepo.save(employee);
     }
 
     @Override
     public Employee update(Employee employee) {
+        log.info("update method " + employee);
         Employee oldemployee = employeeRepo.findById(employee.getId()).orElse(null);
         if (oldemployee != null) {
             oldemployee.setAge(employee.getAge());
@@ -30,6 +38,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public void delete(Employee employee) {
+        log.info("delete method " + employee);
         employeeRepo.deleteById(employee.getId());
     }
 }
